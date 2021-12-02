1510b2887739bd63f54a3a228970c68c1c752412
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-02 12:24:33 -0500

    fix bad usage of promise

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import Test from './test.jsx';
 console.log('hello world!');
 console.log(Test);
 
-const promise = new Promise();
-promise.resolve();
+const promise = new Promise((resolve, reject) => {
+	resolve(555);
+}).then(val => {
+	console.log(val);
+});
 
 const rofl = [1, 2, 3, 4, 5].map(num => {
 	return num + 1;
