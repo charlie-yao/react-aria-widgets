e47bd1bcda3e5a829843ac0731b0be1d7a68500e
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-02 13:10:12 -0500

    some cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Components and Styles
 import Test from './test';
 import './styles.scss';
-
-console.log('hello world!');
-console.log(Test);
-
-const promise = new Promise((resolve, reject) => {
-	resolve(555);
-}).then(val => {
-	console.log(val);
-});
-
-const rofl = [1, 2, 3, 4, 5].map(num => {
-	return num + 1;
-});
-
-console.log(rofl);
-
-console.log(Test);
 
 ReactDOM.render(<Test />, document.getElementById('reactRoot'));
