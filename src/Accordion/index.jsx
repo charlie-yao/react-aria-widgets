56a39650b23c397d10d3ff72f921b59af84b5f24
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-02 18:37:53 -0500

    add some dummy html

--- src/Accordion/index.jsx
+++ src/Accordion/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class Accordion extends React.Component {
 	render() {
 		return (
-			<div>Hello world!</div>
+			<Fragment>
+				<h2><button>Section 1</button></h2>
+				<section>Section 1 content</section>
+				<h2><button>Section 2</button></h2>
+				<section>Section 2 content</section>
+				<h2><button>Section 3</button></h2>
+				<section>Section 3 content</section>
+			</Fragment>
 		);
 	}
 }
