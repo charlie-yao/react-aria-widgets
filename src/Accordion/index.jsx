24725e42bf1a4ad1e2eb2ddd8002e68d47cb7d91
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-03 10:58:48 -0500

    dummy form to test interactive elements

--- src/Accordion/index.jsx
+++ src/Accordion/index.jsx
@@ -51,6 +51,10 @@ class Accordion extends React.Component {
 		}
 	}
 
+	onDummySubmit = (event) => {
+		event.preventDefault();
+	}
+
 	//---- Rendering ----
 	render() {
 		const { allowMultiple, allowToggle } = this.props;
@@ -73,7 +77,13 @@ class Accordion extends React.Component {
 					aria-labelledby="section1Header"
 					hidden={ !section1PanelExpanded }
 				>
-					Section 1 content
+					<form onSubmit={ this.onDummySubmit }>
+						<label htmlFor="section1Input1">Dummy Input #1</label>
+						<input type="text" id="section1Input1" required />
+						<label htmlFor="section1Input2">Dummy Input #2</label>
+						<input type="number" min="0" max="100" required step="1" id="section1Input2"/>
+						<button type="submit">Submit</button>
+					</form>
 				</section>
 				<h2 id="section2Header">
 					<button
