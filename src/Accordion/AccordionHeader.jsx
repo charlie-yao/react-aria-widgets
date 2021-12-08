6fc5d771acbf74412088b40063ad560a683b6b88
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-08 13:13:27 -0500

    dummy AccordionHeader component

---
+++ src/Accordion/AccordionHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function AccordionHeader(props) {
+}
+
+AccordionHeader.propTypes = {
+};
+
+AccordionHeader.defaultProps = {
+};
+
+export default AccordionHeader;
