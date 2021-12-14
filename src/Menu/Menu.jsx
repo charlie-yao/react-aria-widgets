ae6e8f6162f30a4530cf3c5d76e00fceafeed545
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-14 13:58:58 -0500

    dummy Menu component

---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Menu(props) {
+	return (
+		<div>
+			Hello world!
+		</div>
+	);
+}
+
+Menu.propTypes = {
+};
+
+Menu.defaultProps = {
+};
+
+export default Menu;
