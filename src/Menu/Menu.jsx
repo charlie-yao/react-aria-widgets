056e9aa95c65300c9621949a04e4c732f6a763a0
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-14 14:41:38 -0500

    fill out some props and attributes for Menu

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Menu(props) {
+	const { children, orientation } = props;
+	
+	//TODO keyboard support if orientation is horizontal?
 	return (
-		<div>
-			Hello world!
-		</div>
+		<ul
+			role="menu"
+			aria-orientation={ orientation }
+		>
+			{ children }
+		</ul>
 	);
 }
 
 Menu.propTypes = {
+	children: PropTypes.node.isRequired,
+	orientation: PropTypes.oneOf(['vertical', 'horizontal']),
 };
 
 Menu.defaultProps = {
+	orientation: 'vertical',
 };
 
 export default Menu;
