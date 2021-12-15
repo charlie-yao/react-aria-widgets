8d1bc676a84d7c35cb1f25848442f439357609df
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-14 19:08:22 -0500

    make MenuBar a class component for now

--- src/Menu/MenuBar.jsx
+++ src/Menu/MenuBar.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function MenuBar(props) {
-	const { children, orientation, label, labelId } = props;
-
-	return (
-		<ul
-			role="menubar"
-			aria-orientation={ orientation }
-			aria-labelledby={ labelId }
-			aria-label={ label }
-		>
-			{ children }
-		</ul>
-	);
-}
-
-/**
+/*
  * Some notes on props:
  *
  * - If the menubar has a visible label, a labelId prop that points towards
  * the labeling element should be provided. Otherwise, one should pass in
  * a label via the label prop. In other words, one XOR the other must be provided.
  */
-MenuBar.propTypes = {
-	children: PropTypes.node.isRequired,
-	orientation: PropTypes.oneOf([ 'vertical', 'horizontal' ]),
-	label: PropTypes.string, //eslint-disable-line react/require-default-props
-	labelId: PropTypes.string, //eslint-disable-line react/require-default-props
-};
+class MenuBar extends React.Component {
+	static propTypes = {
+		children: PropTypes.node.isRequired,
+		orientation: PropTypes.oneOf([ 'vertical', 'horizontal' ]),
+		label: PropTypes.string, //eslint-disable-line react/require-default-props
+		labelId: PropTypes.string, //eslint-disable-line react/require-default-props
+	};
 
-MenuBar.defaultProps = {
-	orientation: 'horizontal',
-};
+	static defaultProps = {
+		orientation: 'horizontal',
+	};
+
+	//---- Rendering ----
+	render() {
+		const { children, orientation, label, labelId } = this.props;
+
+		return (
+			<ul
+				role="menubar"
+				aria-orientation={ orientation }
+				aria-labelledby={ labelId }
+				aria-label={ label }
+			>
+				{ children }
+			</ul>
+		);
+	}
+}
 
 export default MenuBar;
