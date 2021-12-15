4d4c2ee4459742a70491ee6668a3b42261c92952
Charlie Yao <charlie-yao@users.noreply.github.com>
2021-12-14 19:07:08 -0500

    add a label and labelId prop to MenuBar

--- src/Menu/MenuBar.jsx
+++ src/Menu/MenuBar.jsx
@@ -2,21 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function MenuBar(props) {
-	const { children, orientation } = props;
+	const { children, orientation, label, labelId } = props;
 
 	return (
 		<ul
 			role="menubar"
 			aria-orientation={ orientation }
+			aria-labelledby={ labelId }
+			aria-label={ label }
 		>
 			{ children }
 		</ul>
 	);
 }
 
+/**
+ * Some notes on props:
+ *
+ * - If the menubar has a visible label, a labelId prop that points towards
+ * the labeling element should be provided. Otherwise, one should pass in
+ * a label via the label prop. In other words, one XOR the other must be provided.
+ */
 MenuBar.propTypes = {
 	children: PropTypes.node.isRequired,
 	orientation: PropTypes.oneOf([ 'vertical', 'horizontal' ]),
+	label: PropTypes.string, //eslint-disable-line react/require-default-props
+	labelId: PropTypes.string, //eslint-disable-line react/require-default-props
 };
 
 MenuBar.defaultProps = {
