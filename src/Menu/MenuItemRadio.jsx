a5b2e03678d75a3beb4b00e4417aaeac78f06999
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-01-06 13:49:36 -0500

    menuitemradio implementation

---
+++ src/Menu/MenuItemRadio.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MenuItemRadio = React.forwardRef(function MenuItemRadio(props, ref) {
+	const { children, index, level, onKeyDown, isDisabled, isTabbable, isChecked } = props;
+
+	return (
+		<li
+			role="menuitemcheckbox"
+			data-index={ index }
+			data-level={ level }
+			onKeyDown={ onKeyDown }
+			aria-disabled={ isDisabled }
+			tabIndex={ isTabbable ? '0' : '-1' }
+			aria-checked={ isChecked }
+			ref={ ref }
+		>
+			{ children }
+		</li>
+	);
+});
+
+MenuItemRadio.propTypes = {
+	children: PropTypes.node.isRequired,
+	index: PropTypes.number.isRequired,
+	level: PropTypes.number.isRequired,
+	onKeyDown: PropTypes.func.isRequired,
+	isDisabled: PropTypes.bool,
+	isTabbable: PropTypes.bool,
+	isChecked: PropTypes.bool,
+};
+
+MenuItemRadio.defaultProps = {
+	isDisabled: false,
+	isTabbable: false,
+	isChecked: false,
+};
+
+export default MenuItemRadio;
