d73471d576621ea9c5a7fc8d9285106565920a13
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-01-06 15:31:07 -0500

    add a comment for menuitemradiogroup

--- src/Menu/MenuItemRadioGroup.jsx
+++ src/Menu/MenuItemRadioGroup.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/*
+ * Note that if a menu or menubar contains more than one group of
+ * menuitemradio elements or if the menu has a group of menuitemradio
+ * elements alongside other unrelated menu items:
+ *
+ * - the menuitemradio elements should be nested in an element with the "group" role
+ * - menuitemradio groups should be delimited with an element with the "separator" role
+ *
+ * See:
+ * https://www.w3.org/TR/wai-aria-1.1/#menuitemradio
+ */
 function MenuItemRadioGroup(props) {
 	const { children, label, labelId } = props;
 
