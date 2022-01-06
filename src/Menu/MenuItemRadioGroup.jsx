1b260920598427f00110fb27d8a706265cd40ff8
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-01-06 15:27:32 -0500

    wrap menuitemradiogroup in an <li>

--- src/Menu/MenuItemRadioGroup.jsx
+++ src/Menu/MenuItemRadioGroup.jsx
@@ -5,13 +5,15 @@ function MenuItemRadioGroup(props) {
 	const { children, label, labelId } = props;
 
 	return (
-		<ul
-			role="group"
-			aria-label={ label }
-			aria-labelledby={ labelId }
-		>
-			{ children }
-		</ul>
+		<li>
+			<ul
+				role="group"
+				aria-label={ label }
+				aria-labelledby={ labelId }
+			>
+				{ children }
+			</ul>
+		</li>
 	);
 }
 
