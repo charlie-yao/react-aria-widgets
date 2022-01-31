5b266688f1ec9280116d1f6c1fece6e51284b546
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-01-31 12:23:36 -0500

    convert menu to a class component

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -7,38 +7,41 @@ import PropTypes from 'prop-types';
  * - The menu should either have a labelId prop that points to the menuitem or
  * button that controls its display XOR a label prop.
  */
-function Menu(props) {
-	const { children, orientation, label, labelId, id, className } = props;
+class Menu extends React.Component {
+	static propTypes = {
+		children: PropTypes.node.isRequired,
+		orientation: PropTypes.oneOf([ 'vertical', 'horizontal' ]),
+		label: PropTypes.string,
+		labelId: PropTypes.string,
+		id: PropTypes.string,
+		className: PropTypes.string,
+	};
 
-	return (
-		<ul
-			role="menu"
-			aria-orientation={ orientation }
-			aria-label={ label }
-			aria-labelledby={ labelId }
-			id={ id }
-			className={ className }
-		>
-			{ children }
-		</ul>
-	);
+	static defaultProps = {
+		orientation: 'vertical',
+		label: undefined,
+		labelId: undefined,
+		id: undefined,
+		className: undefined,
+	};
+
+	//---- Rendering ----
+	render() {
+		const { children, orientation, label, labelId, id, className } = this.props;
+
+		return (
+			<ul
+				role="menu"
+				aria-orientation={ orientation }
+				aria-label={ label }
+				aria-labelledby={ labelId }
+				id={ id }
+				className={ className }
+			>
+				{ children }
+			</ul>
+		);
+	}
 }
-
-Menu.propTypes = {
-	children: PropTypes.node.isRequired,
-	orientation: PropTypes.oneOf([ 'vertical', 'horizontal' ]),
-	label: PropTypes.string,
-	labelId: PropTypes.string,
-	id: PropTypes.string,
-	className: PropTypes.string,
-};
-
-Menu.defaultProps = {
-	orientation: 'vertical',
-	label: undefined,
-	labelId: undefined,
-	id: undefined,
-	className: undefined,
-};
 
 export default Menu;
