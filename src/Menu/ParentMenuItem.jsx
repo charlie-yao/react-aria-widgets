9295c3df541a8ac4c317451e7bbc73a39a42c175
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-02-01 19:51:22 -0500

    get rid of a bunch of commented code

--- src/Menu/ParentMenuItem.jsx
+++ src/Menu/ParentMenuItem.jsx
@@ -214,113 +214,6 @@ class _ParentMenuItem extends React.Component {
 			collapse(true);
 	};
 
-	/*
-	onKeyDown = (event) => {
-		const { items, orientation } = this.props;
-		const { key, target } = event;
-		const position = target.dataset.position.split(',');
-		const flattenedPosition = target.dataset.flattenedposition.split(',');
-		const index = Number.parseInt(position[position.length - 1], 10);
-		const flattenedIndex = Number.parseInt(flattenedPosition[flattenedPosition.length - 1], 10);
-		const flattenedRootIndex = Number.parseInt(flattenedPosition[0], 10);
-		const level = position.length - 1;
-		const item = items[index];
-		const { type, onActivate } = item;
-
-		console.log(orientation, position, flattenedPosition, index, flattenedIndex, level, item);
-
-		if(key === 'ArrowUp' || key === 'Up') {
-			event.preventDefault();
-			event.stopPropagation();
-
-			if(orientation === 'vertical') {
-			}
-			else {
-			}
-		}
-		else if(key === 'ArrowDown' || key === 'Down') {
-			event.preventDefault();
-			event.stopPropagation();
-
-			if(orientation === 'vertical') {
-			}
-			else {
-			}
-		}
-		else if(key === 'ArrowLeft' || key === 'Left') {
-			event.preventDefault();
-			event.stopPropagation();
-
-			if(orientation === 'vertical') {
-			}
-			else {
-			}
-		}
-		else if(key === 'ArrowRight' || key === 'Right') {
-			event.preventDefault();
-			event.stopPropagation();
-
-			if(orientation === 'vertical') {
-			}
-			else {
-			}
-		}
-		else if(key === 'Enter') {
-			event.preventDefault();
-			event.stopPropagation();
-
-			if(type === 'menu') {
-			}
-			else if(type === 'checkbox') {
-				if(typeof onActivate === 'function')
-					onActivate(event);
-			}
-			else if(type === 'radiogroup') {
-				if(typeof onActivate === 'function')
-					onActivate(event);
-			}
-			else if(type === 'item') {
-				if(typeof onActivate === 'function')
-					onActivate(event);
-			}
-		}
-		else if(key === ' ' || key === 'Spacebar') {
-			event.preventDefault();
-			event.stopPropagation();
-
-			if(type === 'menu') {
-			}
-			else if(type === 'checkbox') {
-				if(typeof onActivate === 'function')
-					onActivate(event);
-			}
-			else if(type === 'radiogroup') {
-				if(typeof onActivate === 'function')
-					onActivate(event);
-			}
-			else if(type === 'item') {
-				if(typeof onActivate === 'function')
-					onActivate(event);
-			}
-		}
-		else if(key === 'Home') {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-		else if(key === 'End') {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-		else if(key === 'Escape' || key === 'Esc') {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-		else if(key === 'Tab') {
-			event.stopPropagation();
-		}
-	};
-	*/
-
 	//---- Rendering ----
 	render() {
 		const {
@@ -329,7 +222,6 @@ class _ParentMenuItem extends React.Component {
 			isExpanded, isDisabled, isTabbable,
 			setManagerRef,
 		} = this.props;
-		//const { isExpanded } = this.state;
 
 		//console.log(this.props, this.state, this.itemRef, this.childItemRefs);
 
