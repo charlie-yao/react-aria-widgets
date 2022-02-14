009bfe217139a77896df77a8bace15d09e824b89
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-02-14 17:44:33 -0500

    move some code out of <App> into <DemoAccordion>

---
+++ src/DemoAccordion/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+//Components and Styles
+import Accordion from 'src/Accordion';
+
+class DemoAccordion extends React.Component {
+	//---- Events ----
+	onSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	//---- Rendering ----
+	render() {
+		return (
+			<Accordion headerLevel={ 2 } sections={ this.getSections() } />
+		);
+	}
+
+	//---- Misc. ----
+	getSections = () => {
+		return [
+			{
+				id: 'section1',
+				header: 'Section 1',
+				panel: (
+					<form onSubmit={ this.onSubmit }>
+						<label htmlFor="section1Input1">Dummy Input #1</label>
+						<input type="text" id="section1Input1" required />
+						<label htmlFor="section1Input2">Dummy Input #2</label>
+						<input type="number" min="0" max="100" required step="1" id="section1Input2" />
+						<button type="submit">Submit</button>
+					</form>
+				),
+			},
+			{
+				id: 'section2',
+				header: 'Section 2',
+				panel: 'Section 2 content',
+			},
+			{
+				id: 'section3',
+				header: 'Section 3',
+				panel: 'Section 3 content',
+			},
+		];
+	};
+}
+
+export default DemoAccordion;
