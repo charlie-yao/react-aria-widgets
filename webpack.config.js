1371db8acd431cba37ceca4a72b24956d1dec74f
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-02-22 17:42:21 -0500

    rename index.jsx to demoIndex.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 			src: path.resolve(__dirname, 'src/'),
 		},
 	},
-	entry: './src/index.jsx',
+	entry: './src/demoIndex.jsx',
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
