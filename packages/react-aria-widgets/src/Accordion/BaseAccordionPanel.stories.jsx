41cccb447edaa7d8de121eec714758b27831f309
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-03-16 15:10:56 -0400

    stories for BaseAccordionPanel

---
+++ packages/react-aria-widgets/src/Accordion/BaseAccordionPanel.stories.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+//Components and Styles
+import BaseAccordionPanel from './BaseAccordionPanel';
+
+export default {
+	title: 'Accordion/BaseAccordionPanel',
+	component: BaseAccordionPanel,
+};
+
+const Template = (args) => <BaseAccordionPanel {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	children: 'Hello world!',
+	id: 'placeholder',
+	labelId: 'labelPlaceholder',
+};
+
+export const WithSectionRole = Template.bind({});
+WithSectionRole.args = {
+	children: 'Hello world!',
+	id: 'placeholder',
+	labelId: 'labelPlaceholder',
+	tagName: 'div',
+	role: 'section'
+};
+
+export const WithoutSectionRole = Template.bind({});
+WithoutSectionRole.args = {
+	children: 'Hello world!',
+	id: 'placeholder',
+	tagName: 'div',
+};
