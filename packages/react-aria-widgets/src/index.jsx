acf9752f9809c97ed9e9a0a95bef4256fc8bf92c
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-03-15 22:32:02 -0400

    minor inconsequential changes

--- packages/react-aria-widgets/src/index.jsx
+++ packages/react-aria-widgets/src/index.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //Components and Styles
-//import App from 'src/App';
-import 'src/styles.scss';
 import * as Accordion from 'src/Accordion';
-
-//ReactDOM.render(<App />, document.getElementById('reactRoot'));
+import 'src/styles.scss';
 
 export {
 	Accordion,
