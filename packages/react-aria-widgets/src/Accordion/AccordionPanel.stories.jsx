c3b9dc38a9c3df2c9c986677ed9a662b7295f4ff
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-03-21 15:21:05 -0400

    AccordionPanel stories

---
+++ packages/react-aria-widgets/src/Accordion/AccordionPanel.stories.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+//Components and Styles
+import AccordionPanel from 'src/Accordion/AccordionPanel';
+
+export default {
+	title: 'Accordion/AccordionPanel',
+	component: AccordionPanel,
+	args: {
+		children: 'Hello world!',
+		id: 'accordionId',
+	},
+};
+
+const Template = args => <AccordionPanel {...args} />;
+
+export const Expanded = Template.bind({});
+Expanded.args = {
+	isExpanded: true,
+};
+
+export const Collapsed = Template.bind({});
+Collapsed.args = {
+	isExpanded: false,
+};
+
+export const WithClassName = Template.bind({});
+WithClassName.args = {
+	isExpanded: false,
+	className: 'dummyClassName',
+};
