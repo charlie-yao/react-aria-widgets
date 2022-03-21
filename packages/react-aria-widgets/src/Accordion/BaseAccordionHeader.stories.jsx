73f6dc0553049132a945b2e3396310cb1fa4d36b
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-03-21 15:08:45 -0400

    add more BaseAccordionHeader stories and use component-level args

--- packages/react-aria-widgets/src/Accordion/BaseAccordionHeader.stories.jsx
+++ packages/react-aria-widgets/src/Accordion/BaseAccordionHeader.stories.jsx
@@ -6,14 +6,23 @@ import BaseAccordionHeader from 'src/Accordion/BaseAccordionHeader';
 export default {
 	title: 'Accordion/BaseAccordionHeader',
 	component: BaseAccordionHeader,
+	args: {
+		children: 'Hello world!',	
+		id: 'headerId',
+		controlsId: 'panelId',
+		headerLevel: 'h2',
+		onClick: () => {},
+	},
 };
 
 const Template = args => <BaseAccordionHeader {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
-	id: 'headerId',
-	controlsId: 'panelId',
-	headerLevel: 'h2',
-	onClick: () => {},
+export const Expanded = Template.bind({});
+Expanded.args = {
+	isExpanded: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	isDisabled: true,
 };
--- packages/react-aria-widgets/src/Accordion/BaseAccordionPanel.stories.jsx
+++ packages/react-aria-widgets/src/Accordion/BaseAccordionPanel.stories.jsx
@@ -6,29 +6,24 @@ import BaseAccordionPanel from 'src/Accordion/BaseAccordionPanel';
 export default {
 	title: 'Accordion/BaseAccordionPanel',
 	component: BaseAccordionPanel,
+	args: {
+		children: 'Hello world!',
+		id: 'placeholder',
+		labelId: 'labelPlaceholder',
+	},
 };
 
 const Template = args => <BaseAccordionPanel {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-	children: 'Hello world!',
-	id: 'placeholder',
-	labelId: 'labelPlaceholder',
-};
 
 export const WithSectionRole = Template.bind({});
 WithSectionRole.args = {
-	children: 'Hello world!',
-	id: 'placeholder',
-	labelId: 'labelPlaceholder',
 	tagName: 'div',
 	role: 'section'
 };
 
 export const WithoutSectionRole = Template.bind({});
 WithoutSectionRole.args = {
-	children: 'Hello world!',
-	id: 'placeholder',
 	tagName: 'div',
 };
