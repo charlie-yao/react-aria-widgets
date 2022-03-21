e92dfc37176e810297176c67b1fdbf3f664f3eb0
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-03-21 16:05:26 -0400

    AccordionHeader stories

---
+++ packages/react-aria-widgets/src/Accordion/AccordionHeader.stories.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+//Components and Styles
+import AccordionHeader from 'src/Accordion/AccordionHeader';
+
+export default {
+	title: 'Accordion/AccordionHeader',
+	component: AccordionHeader,
+	args: {
+		children: 'Hello world!',
+		id: 'accordionId',
+		index: 1,
+		headerLevel: 'h2',
+		onTriggerClick: () => {},
+		onTriggerKeyDown: () => {},
+		setSectionRef: () => {},
+	},
+};
+
+const Template = args => <AccordionHeader {...args} />;
+
+export const Expanded = Template.bind({});
+Expanded.args = {
+	isExpanded: true,
+};
+
+export const Collapsed = Template.bind({});
+Collapsed.args = {
+	isExpanded: false,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	isExpanded: true,
+	isDisabled: true,
+};
