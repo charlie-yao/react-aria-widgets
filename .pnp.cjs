fae8ba3d4c7cf89a1d8394ab97909c41ceb098dc
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-04-25 16:41:32 -0400

    get rid of eslint-plugin-react

--- .pnp.cjs
+++ .pnp.cjs
@@ -2856,7 +2856,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@charlie-yao/react-aria-widgets", "virtual:edfd7b7aad422d2e680d3e240108e489c6c6f30f594cf930c269ed12e7143e1233f2550f831e3a2ef1579ea236e20452741066e4d568af9bdf075a621e629150#workspace:packages/react-aria-widgets"],
             ["eslint", "npm:8.11.0"],
             ["eslint-config-next", "virtual:edfd7b7aad422d2e680d3e240108e489c6c6f30f594cf930c269ed12e7143e1233f2550f831e3a2ef1579ea236e20452741066e4d568af9bdf075a621e629150#npm:12.1.5"],
-            ["eslint-plugin-react", "virtual:189f1e6b3b875b5ee7a9e02119dfdc23b69e58295d86cd997d7f7507e3d0b4939a3951c14639e2241cdc6fa7b396b6d4dd54f470f8b217e7aeb4c846360ef31f#npm:7.29.3"],
             ["next", "virtual:edfd7b7aad422d2e680d3e240108e489c6c6f30f594cf930c269ed12e7143e1233f2550f831e3a2ef1579ea236e20452741066e4d568af9bdf075a621e629150#npm:12.1.5"],
             ["prop-types", "npm:15.8.1"],
             ["react", "npm:17.0.2"],
