ac65fc4a8cc1cd2999e4fe9eb0eccc23f2b9396d
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-04-27 16:57:36 -0400

    add some more content to the home page

--- packages/react-aria-widgets-demo/pages/index.jsx
+++ packages/react-aria-widgets-demo/pages/index.jsx
@@ -1,7 +1,34 @@
 export default function HomePage() {
 	return (
-		<>
+		<article>
 			<h1>React Aria Widgets</h1>
-		</>
+			<p>
+				React Aria Widgets is a collection of low-level React components designed
+				to help developers implement the design patterns and widgets found
+				in <a href="https://www.w3.org/TR/wai-aria-practices-1.2/">WAI-ARIA Authoring Practices 1.2</a>.
+			</p>
+			<aside>
+				<p>
+					<small>Please note that this library should <strong>NOT</strong> be used in a production environment! 
+					It was developed primarily for educational purposes, and I cannot guarantee longterm
+					support in the form of additional features, bug fixes, maintenance, etc.!</small>
+				</p>
+			</aside>
+			<h2>Features</h2>
+			<ul>
+				<li>
+					Mostly* unstyled and easily customizable
+				</li>
+				<li>
+					Modular components that separate out stateful logic into higher-order components (HOCs).
+				</li>
+				<li>
+					Full keyboard support.
+				</li>
+				<li>
+					Adheres to the <a href="https://www.w3.org/TR/wai-aria-practices-1.2/">WAI-ARIA Authoring Practices 1.2</a>.\
+				</li>
+			</ul>
+		</article>
 	);
 }
