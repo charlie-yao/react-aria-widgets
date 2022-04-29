083810bb7c9f6d2f130654c749e7250c8783b773
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-04-28 20:19:42 -0400

    skeleton for the Accordion page

--- packages/react-aria-widgets-demo/pages/accordion/index.jsx
+++ packages/react-aria-widgets-demo/pages/accordion/index.jsx
@@ -2,6 +2,26 @@ export default function AccordionPage() {
 	return (
 		<article>
 			<h1>Accordion</h1>
+			<p>
+				An <a href="https://www.w3.org/TR/wai-aria-practices-1.2/#accordion">accordion</a> is
+				a collection of vertically-stacked content where each bit of content has an interactive
+				heading and the content itself. The heading is used to control the visibility of the content.
+			</p>
+			<h2>Usage and Examples</h2>
+			<h2>Higher-Order Components and Hooks</h2>
+			<h3><code>withAccordionManager</code></h3>
+			<h2>Components</h2>
+			<h3><code>&lt;Accordion&gt;</code></h3>
+			<h3><code>&lt;AccordionSection&gt;</code></h3>
+			<h3><code>&lt;AccordionHeader&gt;</code></h3>
+			<h3><code>&lt;AccordionPanel&gt;</code></h3>
+			<h2>Base Components</h2>
+			<h3><code>&lt;BaseAccordionHeader&gt;</code></h3>
+			<h3><code>&lt;BaseAccordionPanel&gt;</code></h3>
+			<ul>
+				<li>mention using <code>&lt;section&gt;</code> versus <code>&lt;div&gt;</code></li>
+				<li>where to put render function documentation? in usage and examples? in accordion section?</li>
+			</ul>
 		</article>
 	);
 }
