81c37ce4c10698f23cc0fdfe4da81394cd5af3d1
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-05-02 22:09:47 -0400

    basic custom rendering example

--- packages/react-aria-widgets-demo/pages/accordion/index.jsx
+++ packages/react-aria-widgets-demo/pages/accordion/index.jsx
@@ -24,6 +24,14 @@ const DisableToggleAccordion = dynamic(
 	},
 );
 
+const CustomRenderingAccordion = dynamic(
+	() => import('./CustomRenderingAccordion'),
+	{
+		ssr: false,
+		loading: () => <p>Loading, please wait...</p>,
+	},
+);
+
 export default function AccordionPage() {
 	return (
 		<article>
@@ -160,6 +168,79 @@ export default function DisableToggleAccordion() {
 			<AccordionSection id="disable-toggle-section3">
 				<AccordionHeader>
 					DisableToggleAccordion - Section 3
+				</AccordionHeader>
+				<AccordionPanel>
+					Hello world!
+				</AccordionPanel>
+			</AccordionSection>
+		</Accordion>
+	);
+}`
+				}</code>
+			</pre>
+			<h3>Custom Rendering</h3>
+			<p>
+				Instead of <code>&lt;AccordionHeader&gt;</code> and <code>&lt;AccordionPanel&gt;</code>,
+				a rendering function can be used as the <code>children</code> prop
+				in <code>&lt;AccordionSection&gt;</code>. <code>&lt;AccordionSection&gt;</code> will
+				pass down every prop it receives to the rendering function.
+			</p>
+			<p>
+				<strong>Note to Charlie: expand upon this in "Building Your Own Accordion"?</strong> E.g.,
+				explain that id and index are needed to identify things, document which props are being
+				passed down (possibly useful for custom focus handling instead of using the premade events?)
+			</p>
+			<CustomRenderingAccordion />
+			<pre>
+				<code>{
+`import { Accordion, AccordionSection, AccordionHeader, AccordionPanel } from '@charlie-yao/react-aria-widgets/accordion';
+
+export default function CustomRenderingAccordion() {
+	return (
+		<Accordion headerLevel={ 4 }>
+			<AccordionSection id="custom-rendering-section1">
+				<AccordionHeader>
+					CustomRenderingAccordion - Section 1
+				</AccordionHeader>
+				<AccordionPanel>
+					Hello world!
+				</AccordionPanel>
+			</AccordionSection>
+			<AccordionSection id="custom-rendering-section2">
+				{
+					(args) => {
+						const {
+							id, index, headerLevel, getIsExpanded, getIsDisabled,
+							setSectionRef, onTriggerClick, onTriggerKeyDown,
+						} = args;
+						const isExpanded = getIsExpanded(id);
+						const isDisabled = getIsDisabled(id);
+
+						return (
+							<>
+								<AccordionHeader
+									id={ id }
+									index={ index }
+									headerLevel={ headerLevel }
+									isExpanded={ isExpanded }
+									isDisabled={ isDisabled }
+									setSectionRef={ setSectionRef }
+									onTriggerClick={ onTriggerClick }
+									onTriggerKeyDown={ onTriggerKeyDown }
+								>
+									CustomRenderingAccordion - Section 2
+								</AccordionHeader>
+								<AccordionPanel id={ id } isExpanded={ isExpanded }>
+									Hello world!
+								</AccordionPanel>
+							</>
+						);
+					}
+				}
+			</AccordionSection>
+			<AccordionSection id="custom-rendering-section3">
+				<AccordionHeader>
+					CustomRenderingAccordion - Section 3
 				</AccordionHeader>
 				<AccordionPanel>
 					Hello world!
