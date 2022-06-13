349a5223071fae0e3f25932efce97cb301397064
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-06-13 18:14:22 -0400

    add parenthesis to hoc

--- packages/react-aria-widgets-demo/pages/accordion/index.jsx
+++ packages/react-aria-widgets-demo/pages/accordion/index.jsx
@@ -103,7 +103,7 @@ export default function AccordionPage() {
 			</pre>
 			<h3>Customization</h3>
 			<p>
-				The <code>&lt;Accordion&gt;</code> component uses the <code>withAccordionManager</code> higher-order
+				The <code>&lt;Accordion&gt;</code> component uses the <code>withAccordionManager()</code> higher-order
 				component (HOC), which handles all of the stateful logic for an accordion. This HOC can be
 				used independently of the other accordion components in React Aria Widgets, allowing developers to
 				create their own accordion implementations.
