c0672064573363a7a99d1aebc5279e284a6ec11c
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-07-26 17:28:56 -0400

    update examples.js with lorem filler

--- packages/react-aria-widgets-demo/pages/accordion/examples.js
+++ packages/react-aria-widgets-demo/pages/accordion/examples.js
@@ -9,7 +9,12 @@ export default function BasicAccordion() {
 					BasicAccordion - Section 1
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor lorem eu blandit
+					placerat. Nam interdum metus id molestie elementum. Aenean lorem eros, facilisis eu sem
+					in, porttitor vestibulum massa. Phasellus tempus condimentum hendrerit. Vestibulum sagittis
+					metus arcu, id ultrices tellus cursus nec. In sit amet purus sed dolor tempus condimentum.
+					Curabitur in sapien ex. Cras condimentum quam in facilisis dictum. Mauris eu pulvinar massa.
+					Nulla facilisis et tellus ut pretium.
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="basic-section2">
@@ -17,7 +22,13 @@ export default function BasicAccordion() {
 					BasicAccordion - Section 2
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Vestibulum vestibulum finibus enim, et rutrum nibh fringilla at. Sed varius purus at
+					vestibulum finibus. Nunc interdum neque vitae viverra faucibus. Nullam erat dui, iaculis
+					sed libero eu, hendrerit volutpat nunc. Vestibulum vitae porttitor turpis. Donec consectetur
+					a tellus vitae mollis. Phasellus egestas blandit purus. Duis tempus, nunc vel mollis efficitur,
+					urna mauris condimentum lectus, ac porttitor orci lorem nec leo. Sed augue metus, laoreet in
+					neque eu, pulvinar dignissim nulla. Nulla facilisi. Vestibulum ante ipsum primis in faucibus
+					orci luctus et ultrices posuere cubilia curae;
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="basic-section3">
@@ -25,7 +36,13 @@ export default function BasicAccordion() {
 					BasicAccordion - Section 3
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Curabitur tempus vulputate massa imperdiet feugiat. Aenean eleifend lectus quis lorem
+					condimentum aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+					per inceptos himenaeos. Quisque rhoncus auctor eros a congue. Nullam interdum id nisl id
+					cursus. Quisque et lobortis nisl. Integer pretium lorem lectus, vel imperdiet diam hendrerit
+					in. Sed ac enim pretium, laoreet sem quis, viverra lectus. Sed sagittis faucibus tellus semper
+					molestie. Aliquam in magna est. Suspendisse at pellentesque augue, facilisis accumsan est.
+					Ut iaculis turpis nec gravida consectetur.
 				</AccordionPanel>
 			</AccordionSection>
 		</Accordion>
@@ -43,7 +60,12 @@ export default function DisableMultipleAccordion() {
 					DisableMultipleAccordion - Section 1
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor lorem eu blandit
+					placerat. Nam interdum metus id molestie elementum. Aenean lorem eros, facilisis eu sem
+					in, porttitor vestibulum massa. Phasellus tempus condimentum hendrerit. Vestibulum sagittis
+					metus arcu, id ultrices tellus cursus nec. In sit amet purus sed dolor tempus condimentum.
+					Curabitur in sapien ex. Cras condimentum quam in facilisis dictum. Mauris eu pulvinar massa.
+					Nulla facilisis et tellus ut pretium.
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="disable-multiple-section2">
@@ -51,7 +73,13 @@ export default function DisableMultipleAccordion() {
 					DisableMultipleAccordion - Section 2
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Vestibulum vestibulum finibus enim, et rutrum nibh fringilla at. Sed varius purus at
+					vestibulum finibus. Nunc interdum neque vitae viverra faucibus. Nullam erat dui, iaculis
+					sed libero eu, hendrerit volutpat nunc. Vestibulum vitae porttitor turpis. Donec consectetur
+					a tellus vitae mollis. Phasellus egestas blandit purus. Duis tempus, nunc vel mollis efficitur,
+					urna mauris condimentum lectus, ac porttitor orci lorem nec leo. Sed augue metus, laoreet in
+					neque eu, pulvinar dignissim nulla. Nulla facilisi. Vestibulum ante ipsum primis in faucibus
+					orci luctus et ultrices posuere cubilia curae;
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="disable-multiple-section3">
@@ -59,7 +87,13 @@ export default function DisableMultipleAccordion() {
 					DisableMultipleAccordion - Section 3
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Curabitur tempus vulputate massa imperdiet feugiat. Aenean eleifend lectus quis lorem
+					condimentum aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+					per inceptos himenaeos. Quisque rhoncus auctor eros a congue. Nullam interdum id nisl id
+					cursus. Quisque et lobortis nisl. Integer pretium lorem lectus, vel imperdiet diam hendrerit
+					in. Sed ac enim pretium, laoreet sem quis, viverra lectus. Sed sagittis faucibus tellus semper
+					molestie. Aliquam in magna est. Suspendisse at pellentesque augue, facilisis accumsan est.
+					Ut iaculis turpis nec gravida consectetur.
 				</AccordionPanel>
 			</AccordionSection>
 		</Accordion>
@@ -77,7 +111,12 @@ export default function DisableToggleAccordion() {
 					DisableToggleAccordion - Section 1
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor lorem eu blandit
+					placerat. Nam interdum metus id molestie elementum. Aenean lorem eros, facilisis eu sem
+					in, porttitor vestibulum massa. Phasellus tempus condimentum hendrerit. Vestibulum sagittis
+					metus arcu, id ultrices tellus cursus nec. In sit amet purus sed dolor tempus condimentum.
+					Curabitur in sapien ex. Cras condimentum quam in facilisis dictum. Mauris eu pulvinar massa.
+					Nulla facilisis et tellus ut pretium.
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="disable-toggle-section2">
@@ -85,7 +124,13 @@ export default function DisableToggleAccordion() {
 					DisableToggleAccordion - Section 2
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Vestibulum vestibulum finibus enim, et rutrum nibh fringilla at. Sed varius purus at
+					vestibulum finibus. Nunc interdum neque vitae viverra faucibus. Nullam erat dui, iaculis
+					sed libero eu, hendrerit volutpat nunc. Vestibulum vitae porttitor turpis. Donec consectetur
+					a tellus vitae mollis. Phasellus egestas blandit purus. Duis tempus, nunc vel mollis efficitur,
+					urna mauris condimentum lectus, ac porttitor orci lorem nec leo. Sed augue metus, laoreet in
+					neque eu, pulvinar dignissim nulla. Nulla facilisi. Vestibulum ante ipsum primis in faucibus
+					orci luctus et ultrices posuere cubilia curae;
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="disable-toggle-section3">
@@ -93,7 +138,13 @@ export default function DisableToggleAccordion() {
 					DisableToggleAccordion - Section 3
 				</AccordionHeader>
 				<AccordionPanel>
-					Hello world!
+					Curabitur tempus vulputate massa imperdiet feugiat. Aenean eleifend lectus quis lorem
+					condimentum aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+					per inceptos himenaeos. Quisque rhoncus auctor eros a congue. Nullam interdum id nisl id
+					cursus. Quisque et lobortis nisl. Integer pretium lorem lectus, vel imperdiet diam hendrerit
+					in. Sed ac enim pretium, laoreet sem quis, viverra lectus. Sed sagittis faucibus tellus semper
+					molestie. Aliquam in magna est. Suspendisse at pellentesque augue, facilisis accumsan est.
+					Ut iaculis turpis nec gravida consectetur.
 				</AccordionPanel>
 			</AccordionSection>
 		</Accordion>
@@ -127,7 +178,12 @@ export default function CustomAccordion(props) {
 					CustomAccordion - Section 1	
 				</AccordionHeader>
 				<AccordionPanel className="defaultPanelClass">
-					Hello world!
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor lorem eu blandit
+					placerat. Nam interdum metus id molestie elementum. Aenean lorem eros, facilisis eu sem
+					in, porttitor vestibulum massa. Phasellus tempus condimentum hendrerit. Vestibulum sagittis
+					metus arcu, id ultrices tellus cursus nec. In sit amet purus sed dolor tempus condimentum.
+					Curabitur in sapien ex. Cras condimentum quam in facilisis dictum. Mauris eu pulvinar massa.
+					Nulla facilisis et tellus ut pretium.
 				</AccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="custom-accordion-section2">
@@ -135,7 +191,13 @@ export default function CustomAccordion(props) {
 					CustomAccordion - Section 2
 				</StyledAccordionHeader>
 				<StyledAccordionPanel>
-					Hello world!
+					Vestibulum vestibulum finibus enim, et rutrum nibh fringilla at. Sed varius purus at
+					vestibulum finibus. Nunc interdum neque vitae viverra faucibus. Nullam erat dui, iaculis
+					sed libero eu, hendrerit volutpat nunc. Vestibulum vitae porttitor turpis. Donec consectetur
+					a tellus vitae mollis. Phasellus egestas blandit purus. Duis tempus, nunc vel mollis efficitur,
+					urna mauris condimentum lectus, ac porttitor orci lorem nec leo. Sed augue metus, laoreet in
+					neque eu, pulvinar dignissim nulla. Nulla facilisi. Vestibulum ante ipsum primis in faucibus
+					orci luctus et ultrices posuere cubilia curae;
 				</StyledAccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="custom-accordion-section3">
@@ -143,7 +205,13 @@ export default function CustomAccordion(props) {
 					CustomAccordion - Section 3
 				</CustomAccordionHeader>
 				<CustomAccordionPanel>
-					Hello world!
+					Curabitur tempus vulputate massa imperdiet feugiat. Aenean eleifend lectus quis lorem
+					condimentum aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+					per inceptos himenaeos. Quisque rhoncus auctor eros a congue. Nullam interdum id nisl id
+					cursus. Quisque et lobortis nisl. Integer pretium lorem lectus, vel imperdiet diam hendrerit
+					in. Sed ac enim pretium, laoreet sem quis, viverra lectus. Sed sagittis faucibus tellus semper
+					molestie. Aliquam in magna est. Suspendisse at pellentesque augue, facilisis accumsan est.
+					Ut iaculis turpis nec gravida consectetur.
 				</CustomAccordionPanel>
 			</AccordionSection>
 			<AccordionSection id="custom-accordion-section4">
@@ -419,7 +487,16 @@ export default function renderFunction(props) {
 				className="defaultPanelClass"
 				style={ style }
 			>
-				Hello world!
+				Aenean eu metus sollicitudin, sollicitudin metus vitae, tempor magna.
+				Quisque faucibus massa efficitur lorem consectetur, quis sodales nunc pretium.
+				Nunc rutrum risus id nisl facilisis, eu gravida ex vestibulum. Curabitur varius auctor
+				magna a maximus. Aenean tortor nulla, posuere vitae sollicitudin a, semper vitae nulla.
+				Praesent ullamcorper magna vitae dui faucibus, lacinia cursus augue sagittis. Phasellus
+				elementum tempor iaculis. Vivamus vehicula ex eu nunc volutpat, in ultrices magna finibus.
+				Fusce egestas velit in lobortis vestibulum. Vestibulum sodales venenatis turpis, eu convallis
+				nisi suscipit sit amet. Aenean placerat ullamcorper nulla quis varius. In in consectetur
+				augue, ac consequat sem. Quisque at ultrices arcu, et aliquam purus. Donec varius justo non
+				suscipit fermentum. Etiam a nisl at ipsum imperdiet faucibus.				
 			</section>
 		</>
 	);
