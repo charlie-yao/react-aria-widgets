23225378af711e092c925c003aa7283937b1ff6f
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-08-17 16:25:06 -0400

    get rid of old code

--- packages/react-aria-widgets-demo/components/Header.jsx
+++ packages/react-aria-widgets-demo/components/Header.jsx
@@ -31,34 +31,4 @@ export default function Header(props) {
 			</div>
 		</header>
 	);
-
-	/*
-	return (
-		<header className={ `${styles.Header} navbar` }>
-			<div className="navbar-brand">
-				<Link href="/">
-					<a className="navbar-item">React ARIA Widgets</a>
-				</Link>
-				<a
-					role="button"
-					className="navbar-burger"
-					aria-label="Navigation"
-					aria-expanded="false"
-					onClick={ () => setNavExpanded(!isNavExpanded) }
-				>
-					<span aria-hidden="true" />
-					<span aria-hidden="true" />
-					<span aria-hidden="true" />
-				</a>
-			</div>
-			<div className="navbar-menu">
-				<div className="navbar-end">
-					<a className="navbar-item" href="https://github.com/charlie-yao/react-aria-widgets">
-						GitHub
-					</a>
-				</div>
-			</div>
-		</header>
-	);
-	*/
 }
