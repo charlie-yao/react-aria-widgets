81dfdb95848c19dd0fe5e8251db48115aa94b1f2
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-08-17 22:17:34 -0400

    begin adding headings to nav

--- packages/react-aria-widgets-demo/components/Navigation.jsx
+++ packages/react-aria-widgets-demo/components/Navigation.jsx
@@ -8,6 +8,9 @@ export default function Navigation(props) {
 
 	return (
 		<nav className={ `${styles.Navigation} ${isNavExpanded ? styles.expanded : ''} menu` }>
+			<p className="menu-label">
+				General
+			</p>
 			<ul className="menu-list">
 				<li>
 					<Link href="/">
@@ -20,19 +23,29 @@ export default function Navigation(props) {
 					</Link>
 				</li>
 				<li>
-					<span className="submenu-label">Patterns</span>
-					<ul>
-						<li>
-							<Link href="/accordion">
-								<a>Accordion</a>
-							</Link>
-						</li>
-					</ul>
-				</li>
-				<li>
 					<Link href="/support">
 						<a>Support</a>
 					</Link>
+				</li>
+			</ul>
+			<p className="menu-label">
+				Patterns
+			</p>
+			<ul className="menu-list">
+				<li>
+					<Link href="/accordion">
+						<a>Accordion</a>
+					</Link>
+				</li>
+			</ul>
+			<p className="menu-label">
+				Links
+			</p>
+			<ul className="menu-list">
+				<li>
+					<a href="https://github.com/charlie-yao/react-aria-widgets">
+						GitHub
+					</a>
 				</li>
 			</ul>
 		</nav>
