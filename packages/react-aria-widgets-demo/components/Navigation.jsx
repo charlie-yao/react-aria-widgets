0b3371a143ce90f04f1679648b329171d3100f83
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-08-18 20:03:22 -0400

    bring back nav stickiness and fix nav margins

--- packages/react-aria-widgets-demo/components/Navigation.jsx
+++ packages/react-aria-widgets-demo/components/Navigation.jsx
@@ -8,46 +8,48 @@ export default function Navigation(props) {
 
 	return (
 		<nav className={ `${styles.Navigation} ${isNavExpanded ? styles.expanded : ''} menu` }>
-			<p className="menu-label">
-				General
-			</p>
-			<ul className="menu-list">
-				<li>
-					<Link href="/">
-						<a>Home</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/getting-started">
-						<a>Getting Started</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/support">
-						<a>Support</a>
-					</Link>
-				</li>
-			</ul>
-			<p className="menu-label">
-				Patterns
-			</p>
-			<ul className="menu-list">
-				<li>
-					<Link href="/accordion">
-						<a>Accordion</a>
-					</Link>
-				</li>
-			</ul>
-			<p className="menu-label">
-				Links
-			</p>
-			<ul className="menu-list">
-				<li>
-					<a href="https://github.com/charlie-yao/react-aria-widgets">
-						GitHub
-					</a>
-				</li>
-			</ul>
+			<div className={ styles.NavigationWrapper }>
+				<p className="menu-label">
+					General
+				</p>
+				<ul className="menu-list">
+					<li>
+						<Link href="/">
+							<a>Home</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/getting-started">
+							<a>Getting Started</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/support">
+							<a>Support</a>
+						</Link>
+					</li>
+				</ul>
+				<p className="menu-label">
+					Patterns
+				</p>
+				<ul className="menu-list">
+					<li>
+						<Link href="/accordion">
+							<a>Accordion</a>
+						</Link>
+					</li>
+				</ul>
+				<p className="menu-label">
+					Links
+				</p>
+				<ul className="menu-list">
+					<li>
+						<a href="https://github.com/charlie-yao/react-aria-widgets">
+							GitHub
+						</a>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 }
