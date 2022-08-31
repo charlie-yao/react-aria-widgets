7d831b5e66a3a104f48367b859c45fe9aa8fd2e0
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-08-31 14:42:31 -0400

    add some basic meta tags

--- packages/react-aria-widgets-demo/pages/_document.jsx
+++ packages/react-aria-widgets-demo/pages/_document.jsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const description = 'A collection of React components designed to help implement the ARIA Authoring Practices Guide (APG).';
+
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<meta name="description" content={ description } />
+				<meta property="og:title" content="React ARIA Widgets" />
+				<meta property="og:type" content="website" />
+				<meta property="og:description" content={ description } />
+				{ /*
+				<meta property="og:url" content="https://charlieyao.com/reactariawidgets" />
+				<meta property="og:image" content="" />
+				*/ }
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
