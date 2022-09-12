214ed56195a94a95fe98bae3fa220d5a40a96e72
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-09-12 14:59:19 -0400

    fix linting issues in Header.jsx

--- packages/react-aria-widgets-demo/components/Header.jsx
+++ packages/react-aria-widgets-demo/components/Header.jsx
@@ -1,11 +1,16 @@
+import { useCallback } from 'react';
+import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 //Components and Styles
 import styles from './Header.module.scss';
 
-export default function Header(props) {
+function Header(props) {
 	const { isNavExpanded, setNavExpanded } = props;
-	
+	const onClick = useCallback(() => {
+		setNavExpanded(!isNavExpanded);
+	}, [ setNavExpanded, isNavExpanded ]);
+
 	return (
 		<header className={ styles.Header }>
 			<div className={ styles.HeaderBrand }>
@@ -24,13 +29,20 @@ export default function Header(props) {
 				</a>
 			</div>
 			<div className={ styles.HeaderHamburger }>
-				<button type="button" onClick={ () => setNavExpanded(!isNavExpanded) }>
-					<i aria-hidden="true" className={ `fa-solid ${isNavExpanded ? 'fa-xmark' : 'fa-bars'} `} />
+				<button type="button" onClick={ onClick }>
+					<i aria-hidden="true" className={ `fa-solid ${isNavExpanded ? 'fa-xmark' : 'fa-bars'} ` } />
 					<span className="is-sr-only">
-						{ `${isNavExpanded ? 'Close' : 'Expand' } Navigation` }
+						{ `${isNavExpanded ? 'Close' : 'Expand'} Navigation` }
 					</span>
 				</button>
 			</div>
 		</header>
 	);
 }
+
+Header.propTypes = {
+	isNavExpanded: PropTypes.bool.isRequired,
+	setNavExpanded: PropTypes.func.isRequired,
+};
+
+export default Header;
