e5d75b16dc7c91928515e6c02419b6d6c46c3b0a
Charlie Yao <charlie-yao@users.noreply.github.com>
2022-09-23 19:47:06 -0400

    manually add the base path for favicons and manifest.json

--- packages/react-aria-widgets-demo/pages/_document.jsx
+++ packages/react-aria-widgets-demo/pages/_document.jsx
@@ -31,14 +31,14 @@ export default function Document() {
 				{ /* <meta name="twitter:image:alt" content="" /> */ }
 
 				{ /* Favicons */ }
-				<link rel="alternate icon" href="favicon.ico" sizes="any" />
-				<link rel="icon" href="favicon.svg" type="image/svg+xml" />
-				<link rel="apple-touch-icon" href="favicon-180.png" />
+				<link rel="alternate icon" href="/react-aria-widgets/favicon.ico" sizes="any" />
+				<link rel="icon" href="/react-aria-widgets/favicon.svg" type="image/svg+xml" />
+				<link rel="apple-touch-icon" href="/react-aria-widgets/favicon-180.png" />
 
 				<meta name="theme-color" content="#000000" />
 				<meta name="color-scheme" content="light" />
 
-				<link rel="manifest" href="/manifest.json" />
+				<link rel="manifest" href="/react-aria-widgets/manifest.json" />
 			</Head>
 			<body>
 				<Main />
