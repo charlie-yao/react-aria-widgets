a1036e310750dd112142a3b0c4ca0de922e22a85
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-03-29 18:48:41 -0400

    extend Validator<t> from @types/prop-types instead of making my own type from scratch

--- packages/react-aria-widgets/src/utils/propTypes.ts
+++ packages/react-aria-widgets/src/utils/propTypes.ts
@@ -1,38 +1,34 @@
-interface Props {
-  [key: string]: any;
-}
+import { Validator } from 'prop-types';
 
-export interface CustomPropValidator {
-  (props: Props, propName: string, componentName?: string): Error | null;
-  isRequired?: CustomPropValidator;
+export interface ValidatorWithRequired<T> extends Validator<T> {
+  isRequired?: Validator<T>;
 }
 
 /**
- * Creates a custom React prop type.
- *
- * The inspiration behind this is that this allows
- * marking props with a custom validator as required.
- *
- * This also circumvents an issue with eslint-plugin-react
- * as the rule react/require-default-props will complain
- * if it encounters a custom prop validator.
- * normally cannot mark them as required.
+ * Lets us declare props with custom validators as mandatory in the usual
+ * "PropTypes.isRequired" manner.
+ * 
+ * Though the validator function can just return an error if the prop isn't
+ * supplied, by declaring it as ".isRequired" in a component's propTypes,
+ * we circumvent an issue with eslint-plugin-react. Normally, the rule
+ * react/require-default-props will complain if it encounters a custom prop validator
+ * because without a wrapper like this, they can't be marked as ".isRequired".
  *
  * @see {@link https://github.com/yannickcr/eslint-plugin-react/issues/1020}
  * @see {@link https://github.com/facebook/react/issues/9125#issuecomment-285531461}
  *
- * @param {CustomPropValidator} customPropValidator - A function that returns an Error if the prop is invalid
+ * @param {Validator} validator - A function that returns an Error if the prop is invalid
  * @param {boolean} [isRequired=false] - Determines whether or not an Error should be returned if the prop isn't supplied
- * @returns {CustomPropValidator} - customPropValidator, but checks if the prop exists if isRequired is true
+ * @returns {ValidatorWithRequired} - A validator that automatically checks if the prop exists if isRequired is true
  */
-export function createCustomPropType(customPropValidator: CustomPropValidator, isRequired: boolean = false): CustomPropValidator {
-  return function(props, propName, componentName) {
+export function createValidatorWithRequired<T>(validator: Validator<T>, isRequired: boolean = false): ValidatorWithRequired<T> {
+  return function(props, propName, componentName, location, propFullName) {
     const prop = props[propName];
 
     if(isRequired && (prop === null || prop === undefined))
       return new Error(`${propName} is a required prop.`);
     else
-      return customPropValidator(props, propName, componentName);
+      return validator(props, propName, componentName, location, propFullName);
   };
 }
 
@@ -40,11 +36,11 @@ export function createCustomPropType(customPropValidator: CustomPropValidator, i
  * Because HTML only allows <h1> to <h6>, complain if
  * an invalid headerLevel prop gets passed in.
  *
- * @param {Props} props
+ * @param {object} props
  * @param {string} propName
  * @returns {Error | null}
  */
-function _validateHeaderLevelProp(props: Props, propName: string): Error | null {
+const _validateHeaderLevelProp: Validator<number> = (props, propName) => {
   const headerLevel = props[propName];
 
   if(!Number.isInteger(headerLevel) || headerLevel < 1 || headerLevel > 6)
@@ -53,6 +49,6 @@ function _validateHeaderLevelProp(props: Props, propName: string): Error | null 
   return null;
 }
 
-const validateHeaderLevelProp = createCustomPropType(_validateHeaderLevelProp);
-validateHeaderLevelProp.isRequired = createCustomPropType(_validateHeaderLevelProp, true);
+const validateHeaderLevelProp = createValidatorWithRequired(_validateHeaderLevelProp);
+validateHeaderLevelProp.isRequired = createValidatorWithRequired(_validateHeaderLevelProp, true);
 export { validateHeaderLevelProp };
