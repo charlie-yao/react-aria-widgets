a9967b0e46533f5dc8e5e43aecd1f0c55f82fd6c
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-04-07 22:04:18 -0400

    convert baseaccordionpanel stories to CSF 3 and add typescript types

--- packages/react-aria-widgets/src/Accordion/BaseAccordionPanel.stories.tsx
+++ packages/react-aria-widgets/src/Accordion/BaseAccordionPanel.stories.tsx
@@ -1,34 +1,32 @@
-import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 //Components
 import BaseAccordionPanel from 'src/Accordion/BaseAccordionPanel';
 
-//Types
-import { BaseAccordionPanelProps } from 'src/Accordion/BaseAccordionPanel';
+type Story = StoryObj<typeof BaseAccordionPanel>;
 
-export default {
-  title: 'Accordion/BaseAccordionPanel',
+const meta = {
   component: BaseAccordionPanel,
   args: {
     children: 'Hello world!',
     id: 'placeholder',
     labelId: 'labelPlaceholder',
   },
+} satisfies Meta<typeof BaseAccordionPanel>;
+
+export const Default: Story = {};
+
+export const WithSectionRole: Story = {
+  args: {
+    tagName: 'div',
+    role: 'section',
+  },
 };
 
-function Template(args: BaseAccordionPanelProps) {
-  return <BaseAccordionPanel { ...args } />;
-}
-
-export const Default = Template.bind({});
-
-export const WithSectionRole = Template.bind({});
-WithSectionRole.args = {
-  tagName: 'div',
-  role: 'section',
+export const WithoutSectionRole: Story = {
+  args: {
+    tagName: 'div',
+  },
 };
 
-export const WithoutSectionRole = Template.bind({});
-WithoutSectionRole.args = {
-  tagName: 'div',
-};
+export default meta;
