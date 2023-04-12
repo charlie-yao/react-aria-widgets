0dab68c3d30e7c935290b01c53f1a4d2235cf2ba
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-04-11 21:23:46 -0400

    convert Accordion to a function component

--- packages/react-aria-widgets/src/Accordion/Accordion.tsx
+++ packages/react-aria-widgets/src/Accordion/Accordion.tsx
@@ -46,48 +46,40 @@ const defaultRenderSection: RenderSection = (section, index, props) => {
   );
 };
 
-class Accordion extends React.Component<AccordionProps> {
-  static propTypes = {
-    sections: PropTypes.arrayOf(
-      PropTypes.exact({
-        id: PropTypes.string.isRequired,
-        renderHeader: PropTypes.func.isRequired,
-        renderPanel: PropTypes.func.isRequired,
-      }).isRequired
-    ).isRequired,
-    headerLevel: validateHeaderLevelProp,
-    renderSection: PropTypes.func,
-    //From <AccordionManager>
-    allowMultiple: PropTypes.bool.isRequired,
-    allowToggle: PropTypes.bool.isRequired,
-    getIsExpanded: PropTypes.func.isRequired,
-    getIsDisabled: PropTypes.func.isRequired,
-    toggleSection: PropTypes.func.isRequired,
-    setHeaderRef: PropTypes.func.isRequired,
-    focusHeader: PropTypes.func.isRequired,
-    focusPrevHeader: PropTypes.func.isRequired,
-    focusNextHeader: PropTypes.func.isRequired,
-    focusFirstHeader: PropTypes.func.isRequired,
-    focusLastHeader: PropTypes.func.isRequired,
-  };
+function Accordion(props: AccordionProps) {
+  const {
+    sections,
+    renderSection = defaultRenderSection,
+    ...rest
+  } = props;
 
-  static defaultProps = {
-    headerLevel: 2,
-    renderSection: defaultRenderSection,
-  };
-
-  //---- Rendering ----
-  render() {
-    const {
-      sections,
-      renderSection = defaultRenderSection,
-      ...rest
-    } = this.props;
-
-    return sections.map((section, index) => {
-      return renderSection(section, index, rest);
-    });
-  }
+  return sections.map((section, index) => {
+    return renderSection(section, index, rest);
+  });
 }
+
+Accordion.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      renderHeader: PropTypes.func.isRequired,
+      renderPanel: PropTypes.func.isRequired,
+    }).isRequired
+  ).isRequired,
+  headerLevel: validateHeaderLevelProp,
+  renderSection: PropTypes.func,
+  //From <AccordionManager>
+  allowMultiple: PropTypes.bool.isRequired,
+  allowToggle: PropTypes.bool.isRequired,
+  getIsExpanded: PropTypes.func.isRequired,
+  getIsDisabled: PropTypes.func.isRequired,
+  toggleSection: PropTypes.func.isRequired,
+  setHeaderRef: PropTypes.func.isRequired,
+  focusHeader: PropTypes.func.isRequired,
+  focusPrevHeader: PropTypes.func.isRequired,
+  focusNextHeader: PropTypes.func.isRequired,
+  focusFirstHeader: PropTypes.func.isRequired,
+  focusLastHeader: PropTypes.func.isRequired,
+};
 
 export default withAccordionManager(Accordion);
