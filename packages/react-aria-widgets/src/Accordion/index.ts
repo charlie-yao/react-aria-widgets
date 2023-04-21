63b97bd01e7e8daa8b8ed75a654ea56e624cf9e0
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-04-21 15:10:12 -0400

    get rid of AccordionStories

--- packages/react-aria-widgets/src/Accordion/index.ts
+++ packages/react-aria-widgets/src/Accordion/index.ts
@@ -1,6 +1,5 @@
 //Components and Styles
 import Accordion from 'src/Accordion/Accordion';
-import AccordionSection from 'src/Accordion/AccordionSection';
 import AccordionHeader from 'src/Accordion/AccordionHeader';
 import AccordionPanel from 'src/Accordion/AccordionPanel';
 import BaseAccordionHeader from 'src/Accordion/BaseAccordionHeader';
@@ -11,7 +10,6 @@ import withAccordionManager from 'src/Accordion/withAccordionManager';
 
 export {
   Accordion,
-  AccordionSection,
   AccordionHeader,
   AccordionPanel,
   BaseAccordionHeader,
