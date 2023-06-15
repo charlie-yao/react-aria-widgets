7bd1677d1d9db8336bb040c42ddbf5120c1dcb75
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-06-15 01:18:21 -0400

    remove older forwardRef example

--- packages/react-aria-widgets/src/Accordion/test.tsx
+++ packages/react-aria-widgets/src/Accordion/test.tsx
@@ -44,21 +44,6 @@ type PanelProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<
   'section' | 'div' | 'button' | 'a' | 'form' | 'ul' | 'input'
 >;
 
-type PanelComponent = <C extends React.ElementType = 'section'>(
-  props: PanelProps<C>
-) => React.ReactElement | null;
-
-/*
-const Panel: PanelComponent = React.forwardRef(<C extends React.ElementType = 'section'>(
-  { as, children,  ...rest }: PanelProps<C>,
-  ref?: PolymorphicRef<C>
-) => {
-  const Component = as || 'section';
-  
-  return <Component {...rest} ref={ ref }>{ children }</Component>
-});
-*/
-
 function Panel<C extends React.ElementType = 'section'>(
   { as, children, ...rest }: PanelProps<C>,
   ref: React.ForwardedRef<HTMLElement>
@@ -96,7 +81,12 @@ function App() {
       <Panel as="bdiv" id="test" labelId="testLabel" href="#">dd</Panel>
       <ForwardedPanel ref={testSectionRef} as="section" id="test" labelId="testLabel">hello world!</ForwardedPanel>
       <ForwardedPanel ref={testButtonRef} as="section" id="test" labelId="testLabel" type="button">dd</ForwardedPanel>
+
+      { /* The type checking should be stricter here */ }
+      <ForwardedPanel ref={testButtonRef} as="section" id="test" labelId="testLabel">dd</ForwardedPanel>
+
       <ForwardedPanel ref={testButtonRef} as="button" id="test" labelId="testLabel" type="button">dd</ForwardedPanel>
+      <ForwardedPanel ref={testButtonRef} as="div" id="test" labelId="testLabel" type="button">dd</ForwardedPanel>
       <ForwardedPanel ref={testButtonRef} as="button" id="test" labelId="testLabel" type="asdfsubmit">dd</ForwardedPanel>
       <ForwardedPanel ref={testDivRef} as="div" id="test" labelId="testLabel" href="#">dd</ForwardedPanel>
       <ForwardedPanel ref={testFormRef} as="a" id="test" labelId="testLabel" href="#">dd</ForwardedPanel>
