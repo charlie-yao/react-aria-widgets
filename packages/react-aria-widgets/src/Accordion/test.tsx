de36b850aa605aeff42a883580de0bd96b5f23c7
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-06-22 01:18:30 -0400

    try writing a helper type that TS will infer the result of forwardRef as without using a type augmentation AND lets us assign properties like defaultProps without type assertions

--- packages/react-aria-widgets/src/Accordion/test.tsx
+++ packages/react-aria-widgets/src/Accordion/test.tsx
@@ -1,10 +1,24 @@
 import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
 
+/*
 declare module "react" {
   function forwardRef<T, P = {}>(
     render: (props: P, ref: React.ForwardedRef<T>) => React.ReactNode
   ): (props: P & React.RefAttributes<T>) => React.ReactElement
-}
+}*/
+
+const VALID_PANEL_ELEMENTS = [
+  'section',
+  'div',
+  'button',
+  'a',
+  'form',
+  'ul',
+  'input',
+] as const;
+
+type ValidPanelElements = typeof VALID_PANEL_ELEMENTS[number];
 
 type AllowedElements<
   C extends React.ElementType,
@@ -38,43 +52,60 @@ type PolymorphicComponentPropsWithRef<
   V extends React.ElementType = React.ElementType
 > = PolymorphicComponentPropsWithoutRef<C, P, V> & { ref?: PolymorphicRef<C> }
 
-type PanelProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<
-  C,
-  {
-    id: string;
-    labelId: string;
-  },
-  'section' | 'div' | 'button' | 'a' | 'form' | 'ul' | 'input'
->;
+interface PolymorphicForwardedComponent<
+  Props,
+  V extends React.ElementType = React.ElementType
+> {
+  <C extends React.ElementType = 'section'>(
+    props: PolymorphicComponentPropsWithRef<C, Props, V>
+  ): React.ReactElement | null;
+
+  defaultProps?: Partial<PolymorphicComponentPropsWithRef<React.ElementType, Props, V>> | undefined;
+  propTypes?: React.WeakValidationMap<PolymorphicComponentPropsWithRef<React.ElementType, Props, V>> | undefined;
+  displayName?: string | undefined;
+}
+
+type PanelProps = {
+  id: string;
+  labelId: string;
+  lol: string;
+};
 
 function Panel<E, C extends React.ElementType = 'section'>(
-  { as, children, ...rest }: PanelProps<C>,
+  { as, children, ...rest }: PolymorphicComponentPropsWithRef<C, PanelProps, ValidPanelElements>,
   ref: React.ForwardedRef<E>
 ) {
   const Component = as ? as : 'section';
   return <Component { ...rest } ref={ ref }>{children}</Component>;
 }
 
-type ForwardedPanelType = <C extends React.ElementType = 'section'>(
-  props: PanelProps<C>
-) => React.ReactElement | null;
+const ForwardedPanel: PolymorphicForwardedComponent<PanelProps, ValidPanelElements> = React.forwardRef(Panel);
 
-//const ForwardedPanel: ForwardedPanelType = React.forwardRef(Panel);
-const ForwardedPanel = React.forwardRef(Panel);
+ForwardedPanel.propTypes = {
+  as: PropTypes.oneOf(VALID_PANEL_ELEMENTS),
+  lol: PropTypes.number.isRequired,
+};
 
+ForwardedPanel.defaultProps = {
+  as: 'error!',
+};
+
+//How reusable is the polymorphic component?
+type WrappedForwardedPanelProps = {
+} & React.ComponentPropsWithRef<typeof ForwardedPanel>;
+
+function WrappedForwardedPanel({ ...rest }: WrappedForwardedPanelProps) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  return <ForwardedPanel {...rest} />;
+}
+
+//Non-generic usage of React.forwardRef
 const Blah = React.forwardRef<HTMLDivElement, React.PropsWithChildren<{ name: string }>>((
   { name, children },
   ref
 ) => {
   return <div ref={ ref } id={ name }>{ children }</div>
 });
-
-//(ForwardedPanel as React.ForwardRefExoticComponent<React.PropsWithoutRef<{ id: string, labelId: string }> & React.RefAttributes<HTMLElement>>).propTypes = {
-(ForwardedPanel as React.ComponentType).propTypes = {
-};
-
-(ForwardedPanel as React.ComponentType).defaultProps = {
-};
 
 function App() {
   const testSectionRef = useRef<HTMLElement>(null);
