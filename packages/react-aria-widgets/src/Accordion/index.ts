db663938c7cb2d989addcf0502210c2845e809b8
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-07-07 22:00:07 -0400

    fix some exports in accordion"s index.ts

--- packages/react-aria-widgets/src/Accordion/index.ts
+++ packages/react-aria-widgets/src/Accordion/index.ts
@@ -1,12 +1,18 @@
 //Components and Styles
 export { default as Accordion } from 'src/Accordion/Accordion';
+export { default as AccordionSection } from 'src/Accordion/AccordionSection';
 export { default as AccordionHeader } from 'src/Accordion/AccordionHeader';
 export { default as AccordionPanel } from 'src/Accordion/AccordionPanel';
 export { default as BaseAccordionHeader } from 'src/Accordion/BaseAccordionHeader';
 export { default as BaseAccordionPanel } from 'src/Accordion/BaseAccordionPanel';
 
-//Types
-export * from 'src/Accordion/propTypes';
+//Contexts
+export { default as AccordionContext } from 'src/Accordion/AccordionContext';
+export { default as AccordionSectionContext } from 'src/Accordion/AccordionSectionContext';
+
+//Hooks
+export { default as useAccordionContext } from 'src/Accordion/useAccordionContext';
+export { default as useAccordionSectionContext } from 'src/Accordion/useAccordionSectionContext';
 
 //Misc.
 export * from 'src/Accordion/utils';
