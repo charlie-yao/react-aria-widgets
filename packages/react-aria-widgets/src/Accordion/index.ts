de2fd7f2e1aea398936063d4af8c93472b205a2c
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-07-25 22:30:34 -0400

    add missing exports to index.ts

--- packages/react-aria-widgets/src/Accordion/index.ts
+++ packages/react-aria-widgets/src/Accordion/index.ts
@@ -1,5 +1,6 @@
 //Components and Styles
 export { default as Accordion } from 'src/Accordion/Accordion';
+export { default as ControlledAccordion } from 'src/Accordion/ControlledAccordion';
 export { default as AccordionSection } from 'src/Accordion/AccordionSection';
 export { default as AccordionHeader } from 'src/Accordion/AccordionHeader';
 export { default as AccordionPanel } from 'src/Accordion/AccordionPanel';
@@ -7,12 +8,16 @@ export { default as BaseAccordionHeader } from 'src/Accordion/BaseAccordionHeade
 export { default as BaseAccordionPanel } from 'src/Accordion/BaseAccordionPanel';
 
 //Contexts
-export { default as AccordionContext } from 'src/Accordion/AccordionContext';
-export { default as AccordionSectionContext } from 'src/Accordion/AccordionSectionContext';
+export { default as AccordionContext, AccordionProvider } from 'src/Accordion/AccordionContext';
+export { default as AccordionSectionContext, AccordionSectionProvider } from 'src/Accordion/AccordionSectionContext';
 
 //Hooks
+export { default as useAccordion } from 'src/Accordion/useAccordion';
 export { default as useAccordionContext } from 'src/Accordion/useAccordionContext';
 export { default as useAccordionSectionContext } from 'src/Accordion/useAccordionSectionContext';
+
+//Types
+export type * from 'src/Accordion/types';
 
 //Misc.
 export * from 'src/Accordion/utils';
