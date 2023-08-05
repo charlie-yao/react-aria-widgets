e5f8b37844af8bc137eac1e45b8a0709e2346930
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-08-05 16:19:36 -0400

    rename in index file

--- packages/react-aria-widgets/src/Accordion/index.ts
+++ packages/react-aria-widgets/src/Accordion/index.ts
@@ -1,7 +1,7 @@
 //Components and Styles
 export { default as Accordion } from 'src/Accordion/Accordion';
 export { default as ControlledAccordion } from 'src/Accordion/ControlledAccordion';
-export { default as AccordionSection } from 'src/Accordion/AccordionSection';
+export { default as AccordionItem } from 'src/Accordion/AccordionItem';
 export { default as AccordionHeader } from 'src/Accordion/AccordionHeader';
 export { default as AccordionPanel } from 'src/Accordion/AccordionPanel';
 export { default as BaseAccordionHeader } from 'src/Accordion/BaseAccordionHeader';
@@ -9,12 +9,12 @@ export { default as BaseAccordionPanel } from 'src/Accordion/BaseAccordionPanel'
 
 //Contexts
 export { default as AccordionContext, AccordionProvider } from 'src/Accordion/AccordionContext';
-export { default as AccordionSectionContext, AccordionSectionProvider } from 'src/Accordion/AccordionSectionContext';
+export { default as AccordionItemContext, AccordionItemProvider } from 'src/Accordion/AccordionItemContext';
 
 //Hooks
 export { default as useAccordion } from 'src/Accordion/useAccordion';
 export { default as useAccordionContext } from 'src/Accordion/useAccordionContext';
-export { default as useAccordionSectionContext } from 'src/Accordion/useAccordionSectionContext';
+export { default as useAccordionItemContext } from 'src/Accordion/useAccordionItemContext';
 
 //Types
 export type * from 'src/Accordion/types';
