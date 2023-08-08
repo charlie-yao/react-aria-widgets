84492bce35f821f6a3d73e7fd82e82c73890a738
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-08-07 22:39:49 -0400

    get rid of that really old src folder

--- packages/react-aria-widgets-demo/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//Components and Styles
-import App from 'src/App';
-
-ReactDOM.render(<App />, document.getElementById('reactRoot'));
