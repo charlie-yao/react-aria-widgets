44e9409038f7fb88e444607d1cf72d950e3fcad6
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-08-10 19:44:17 -0400

    initial shot at revamping the home page

--- packages/react-aria-widgets-demo/pages/index.tsx
+++ packages/react-aria-widgets-demo/pages/index.tsx
@@ -5,34 +5,22 @@ export default function HomePage() {
     <article className="container is-max-desktop content">
       <h1>React ARIA Widgets</h1>
       <p>
-        React ARIA Widgets is a collection of React components designed
+        React ARIA Widgets is a collection of React primitives designed
         to help developers implement the patterns found in
         the <a href="https://www.w3.org/WAI/ARIA/apg/">ARIA Authoring Practices Guide (APG)</a>.
       </p>
       <p className="notification is-light is-info">
-        Please note that this library should <strong>NOT</strong> be used in a production environment!
-        It was developed primarily for educational purposes, and longterm
-        support in the form of additional features, bug fixes, maintenance, or etc. cannot
-        be guaranteed!
+        Please note that this library should <strong>NOT</strong> be used in a production environment! It's
+        still in a pre-alpha stage and the API is subject to major breaking changes.
       </p>
       <h2>Features</h2>
       <ul>
         <li>
-          <Link href="/support/#faq-mostly-unstyled">(Mostly) unstyled</Link> components
-          that are easily composable and customizable.
+          Unstyled, accessible components and hooks that provide functionality and are easily composable and customizable
         </li>
         <li>
-          Modular design that abstracts stateful logic into higher-order components (HOCs) and React hooks.
-        </li>
-        <li>
-          Unopinionated
-          { ' ' }
-          <Link href="/getting-started#base-components">
-            base components
-          </Link>
-          { ' ' }
-          with PropTypes that dictate the necessary HTML/ARIA attributes
-          for developers who need more fine-tuned control.
+          Unopinionated stateless components that provide typings to dictate the necessary HTML/ARIA attributes
+          for developers who need greater control
         </li>
         <li>
           Adheres to the APG complete with focus control and full keyboard support.
