3c62c4b2a46d0ece15710315169dba745dfcc916
Charlie Yao <charlie-yao@users.noreply.github.com>
2023-08-12 21:32:15 -0400

    dynamically render the render props example

--- packages/react-aria-widgets-demo/components/accordion/RenderPropAccordion.tsx
+++ packages/react-aria-widgets-demo/components/accordion/RenderPropAccordion.tsx
@@ -6,49 +6,32 @@ import withStyleWrapper from './withStyleWrapper';
 //Types
 import { AccordionProps } from 'react-aria-widgets/accordion';
 
+const ITEMS = [ 'item1', 'item2', 'item3' ];
+
 function RenderPropAccordion(props: AccordionProps) {
   return (
     <Accordion { ...props }>
-      <AccordionItem id="item1">
-        <AccordionHeader>
-          { ({ id, getIsExpanded }) => (
-            <>
-              Accordion Item 1: Expanded = <code>{ getIsExpanded(id).toString() }</code>
-            </>
-          ) }
-        </AccordionHeader>
-        <AccordionPanel>
-          { ({ id, headerLevel, allowMultiple, allowCollapseLast, getIsExpanded }) => (
-            <ul>
-              <li><code>id</code> = <code>{ id }</code></li>
-              <li><code>headerLevel</code> = <code>{ headerLevel }</code></li>
-              <li><code>allowMultiple</code> = <code>{ allowMultiple.toString() }</code></li>
-              <li><code>allowCollapseLast</code> = <code>{ allowCollapseLast.toString() }</code></li>
-              <li><code>getIsExpanded('item2')</code> = <code>{ getIsExpanded('item2').toString() }</code></li>
-            </ul>
-          ) }
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem id="item2">
-        <AccordionHeader>
-          { ({ id, getIsExpanded }) => (
-            <>
-              Accordion Item 2: Expanded = <code>{ getIsExpanded(id).toString() }</code>
-            </>
-          ) }
-        </AccordionHeader>
-        <AccordionPanel>
-          { ({ id, headerLevel, allowMultiple, allowCollapseLast, getIsExpanded }) => (
-            <ul>
-              <li><code>id</code> = <code>{ id }</code></li>
-              <li><code>headerLevel</code> = <code>{ headerLevel }</code></li>
-              <li><code>allowMultiple</code> = <code>{ allowMultiple.toString() }</code></li>
-              <li><code>allowCollapseLast</code> = <code>{ allowCollapseLast.toString() }</code></li>
-              <li><code>getIsExpanded('item1')</code> = <code>{ getIsExpanded('item1').toString() }</code></li>
-            </ul>
-          ) }
-        </AccordionPanel>
-      </AccordionItem>
+      { ITEMS.map((id, index) => (
+        <AccordionItem key={ id } id={ id }>
+          <AccordionHeader>
+            { ({ getIsExpanded }) => (
+              <>
+                Accordion Item { index + 1 }: Expanded = <code>{ getIsExpanded(id).toString() }</code>
+              </>
+            ) }
+          </AccordionHeader>
+          <AccordionPanel>
+            { ({ headerLevel, allowMultiple, allowCollapseLast }) => (
+              <ul>
+                <li><code>id</code> = <code>{ id }</code></li>
+                <li><code>headerLevel</code> = <code>{ headerLevel }</code></li>
+                <li><code>allowMultiple</code> = <code>{ allowMultiple.toString() }</code></li>
+                <li><code>allowCollapseLast</code> = <code>{ allowCollapseLast.toString() }</code></li>
+              </ul>
+            ) }
+          </AccordionPanel>
+        </AccordionItem>
+      )) }
     </Accordion>
   );
 }
--- packages/react-aria-widgets-demo/pages/accordion/index.tsx
+++ packages/react-aria-widgets-demo/pages/accordion/index.tsx
@@ -176,49 +176,32 @@ export default DisableBothAccordion() {
 const RENDER_PROP_ACCORDION_EXAMPLE =
 `import { Accordion, AccordionItem, AccordionHeader, AccordionPanel } from 'react-aria-widgets/accordion';
 
-export default function RenderPropAccordion() {
+const ITEMS = [ 'item1', 'item2', 'item3' ];
+
+function RenderPropAccordion() {
   return (
-    <Accordion headerLevel={ 4 }>
-      <AccordionItem id="item1">
-        <AccordionHeader>
-          { ({ id, getIsExpanded }) => (
-            <>
-              Accordion Item 1: Expanded = <code>{ getIsExpanded(id).toString() }</code>
-            </>
-          ) }
-        </AccordionHeader>
-        <AccordionPanel>
-          { ({ id, headerLevel, allowMultiple, allowCollapseLast, getIsExpanded }) => (
-            <ul>
-              <li><code>id</code> = <code>{ id }</code></li>
-              <li><code>headerLevel</code> = <code>{ headerLevel }</code></li>
-              <li><code>allowMultiple</code> = <code>{ allowMultiple.toString() }</code></li>
-              <li><code>allowCollapseLast</code> = <code>{ allowCollapseLast.toString() }</code></li>
-              <li><code>getIsExpanded('item2')</code> = <code>{ getIsExpanded('item2').toString() }</code></li>
-            </ul>
-          ) }
-        </AccordionPanel>
-      </AccordionItem>
-      <AccordionItem id="item2">
-        <AccordionHeader>
-          { ({ id, getIsExpanded }) => (
-            <>
-              Accordion Item 2: Expanded = <code>{ getIsExpanded(id).toString() }</code>
-            </>
-          ) }
-        </AccordionHeader>
-        <AccordionPanel>
-          { ({ id, headerLevel, allowMultiple, allowCollapseLast, getIsExpanded }) => (
-            <ul>
-              <li><code>id</code> = <code>{ id }</code></li>
-              <li><code>headerLevel</code> = <code>{ headerLevel }</code></li>
-              <li><code>allowMultiple</code> = <code>{ allowMultiple.toString() }</code></li>
-              <li><code>allowCollapseLast</code> = <code>{ allowCollapseLast.toString() }</code></li>
-              <li><code>getIsExpanded('item1')</code> = <code>{ getIsExpanded('item1').toString() }</code></li>
-            </ul>
-          ) }
-        </AccordionPanel>
-      </AccordionItem>
+    <Accordion headerlevel={ 4 }>
+      { ITEMS.map((id, index) => (
+        <AccordionItem key={ id } id={ id }>
+          <AccordionHeader>
+            { ({ getIsExpanded }) => (
+              <>
+                Accordion Item { index + 1 }: Expanded = <code>{ getIsExpanded(id).toString() }</code>
+              </>
+            ) }
+          </AccordionHeader>
+          <AccordionPanel>
+            { ({ headerLevel, allowMultiple, allowCollapseLast }) => (
+              <ul>
+                <li><code>id</code> = <code>{ id }</code></li>
+                <li><code>headerLevel</code> = <code>{ headerLevel }</code></li>
+                <li><code>allowMultiple</code> = <code>{ allowMultiple.toString() }</code></li>
+                <li><code>allowCollapseLast</code> = <code>{ allowCollapseLast.toString() }</code></li>
+              </ul>
+            ) }
+          </AccordionPanel>
+        </AccordionItem>
+      )) }
     </Accordion>
   );
 }`;
